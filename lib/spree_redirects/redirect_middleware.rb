e92ef0b1f1980a7bbcb900158c757ce3cba52982
Thomas Boltze <thomas.boltze@gmail.com>
2012-11-28 14:51:43 +0800

    Fixed redircts so that they actually work (backport middleware from master)

--- lib/spree_redirects/redirect_middleware.rb
+++ lib/spree_redirects/redirect_middleware.rb
@@ -5,7 +5,33 @@ module SpreeRedirects
       @app = app
     end
    
+
     def call(env)
+      # when consider_all_requests_local is false, an exception is raised for 404
+      # consider_all_requests_local should be false in a production environment
+
+      begin
+        status, headers, body = @app.call(env)
+      rescue Exception => e
+        routing_error = e
+      end
+
+      if routing_error.present? or status == 404
+        path = [ env["PATH_INFO"], env["QUERY_STRING"] ].join("?").sub(/[\/\?\s]*$/, "").strip
+
+        if url = find_redirect(path)
+          # Issue a "Moved permanently" response with the redirect location
+          return [ 301, { "Location" => url }, [ "Redirecting..." ] ]
+        end
+      end
+
+      raise routing_error if routing_error.present?
+
+      [ status, headers, body ]
+    end
+
+
+    def call_original(env)
       status, headers, body = @app.call(env)
       path = [ env["PATH_INFO"], env["QUERY_STRING"] ].join("?").sub(/[\/\?\s]*$/, "").strip
       if status == 404 && url = find_redirect(path)
